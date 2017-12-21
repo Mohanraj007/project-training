f53625a85cc6509dbdf9155b6b28f5ebd3972eee
Mohanraj007 <34641612+Mohanraj007@users.noreply.github.com>
2017-12-21 15:19:05 +0530

    Create power_2.c

---
+++ c/power_2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a,i;
+	scanf("%d",&a);
+	i=2;
+	while(i<=a)
+	{
+		if(i==a)
+		{
+			printf("Yes");
+			break;
+		}
+		i=i*2;
+	}
+	if(i>a)
+	{
+		printf("No");
+	}
+	return 0;
+}
